c2bf973de6004cb5400c8fc1bd96599ce14a1b01
NAGARJUNA ETHAMUKKALA <120629706+NagaRjuna44@users.noreply.github.com>
2024-02-08 20:20:18 +0530

    Add files via upload

---
+++ BANK BALANCE 1.py
@@ -0,0 +1,17 @@
+savings_account_balance = 10000
+print("the toatal balance", savings_account_balance)
+
+savings_account_balance = savings_account_balance + 5000
+print("available balance is =", savings_account_balance)
+
+savings_account_balance =savings_account_balance - 1000
+print("available balance is =", savings_account_balance)
+
+savings_account_balance = savings_account_balance + 1000
+print("available balance is =", savings_account_balance)
+
+savings_account_balance = savings_account_balance - 6000
+print("available balance is =", savings_account_balance)
+
+print("10% intrest = ",savings_account_balance*1*10//100)
+print("after adding the 10% of intrest available balance is =", savings_account_balance*1*10//100+savings_account_balance)
---
+++ variables.py
@@ -0,0 +1,13 @@
+nag=100
+print(nag)
+nag="chandu"
+print(nag)
+nag=2*2+2
+print(nag)
+nag=100
+nag1=200
+print(nag+nag1)
+nag1= 100
+nag2= 200
+print(f"1 month mess bill {nag1}+{nag2}={nag1+nag2}")
+
