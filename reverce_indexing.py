ad75d140eef05cfe96bfd245ee68fa8eca2bd214
NAGARJUNA ETHAMUKKALA <120629706+NagaRjuna44@users.noreply.github.com>
2024-02-09 20:05:34 +0530

    reverce_indexing

---
+++ reverce_indexing.py
@@ -0,0 +1,8 @@
+a = " DELL"
+b = " TECHNOLOGIES AI TRAINING"
+c = " CENTER"
+
+print(c[3:]+b[0:]+c[0:3]+a[::-1])
+
+a = " DELL TECHNOLOGIES AI SKILL TRAINING CENTER"
+print(a[39:]+a[5:39]+a[-39:0:-1])
