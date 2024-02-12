529ce217f37cee695ce4eaee14f07dd5065c54bd
NAGARJUNA ETHAMUKKALA <120629706+NagaRjuna44@users.noreply.github.com>
2024-02-12 18:41:55 +0530

    Add files via upload

---
+++ slicing_and_indexing.py
@@ -0,0 +1,30 @@
+name = "NAGARJUNA"
+print(name)
+print(name[0])
+print(name[-9])
+print(name[0:6])
+print(name[0:4:2])
+print(name[-4:-1])
+print(name[4:])
+print(name[:6])
+
+name = "123456789"
+print(name)
+print(name[0])
+print(name[-9])
+print(name[0:6])
+print(name[0:4:2])
+print(name[-4:-1])
+print(name[4:])
+print(name[:6])
+
+firstname = "sumanth"
+secondname = "sam"
+
+print(firstname[2:4]+secondname[1:3])
+
+name = "JAVASCRIPT"
+name2= "PYTHON"
+print(name[:-2]+name2[-2:])
+print(name2[:-2]+name[-2:])
+
