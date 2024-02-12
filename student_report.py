a1e57381afeb94c2bfdbc4cae422e7aa16a7f7f1
NAGARJUNA ETHAMUKKALA <120629706+NagaRjuna44@users.noreply.github.com>
2024-02-12 18:43:24 +0530

    Add files via upload

---
+++ student_report.py
@@ -0,0 +1,16 @@
+telugu = 90
+english = 90
+hindi = 80
+science = 85
+kannada = 80
+maths = 90
+
+number_of_subjects = 6
+total_score = 600
+total_marks = telugu+english+hindi+science+kannada+maths
+total_percentage = total_marks/total_score*100
+average = total_marks/number_of_subjects
+print(total_marks)
+print(total_percentage)
+print(average)
+print(f"nagarjuna scored {515} with = {515/6}%")
