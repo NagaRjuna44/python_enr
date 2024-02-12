d9783097c0782558db606d9836073ea7a0c7fbb0
NAGARJUNA ETHAMUKKALA <120629706+NagaRjuna44@users.noreply.github.com>
2024-02-12 18:41:02 +0530

    Add files via upload

---
+++ MULTIPLE_VARIABLES_ASSIGNED_MULTIPLE_VALUE.py
@@ -0,0 +1,6 @@
+table1,table2,table3,table4, = 10,20,30,40
+print("mutiple variable assigned to different values")
+print(table1)
+print(table2)
+print(table3)
+print(table4)
---
+++ MULTIPLE_VARIABLES_ASSIGNED_SINGLE_VALUE.py
@@ -0,0 +1,9 @@
+table1=table2=table3=table4 = 10
+print(table1)
+print(table2)
+print(table3)
+print(table4)
+print(table1+table2)
+print(table1+table2+table3)
+print(table1+table2+table3+table4)
+
