56de3335f6ee06dd0a54b430b408de4cc042386e
NAGARJUNA ETHAMUKKALA <120629706+NagaRjuna44@users.noreply.github.com>
2024-02-21 20:06:45 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,90 @@
+#1.NUMBERS AND ALPHABET IN ASCENDING & DESCENDING ORDER
+# a = [1,5,9,2,4,3,8,6]
+# a.sort()
+# print(a)
+#
+# b = [1,5,9,2,4,3,8,6]
+# b.sort(reverse=True)
+# print(b)
+#
+# c = ["a","h","s","l","e","y","z"]
+# c.sort()
+# print(c)
+#
+# d = ["a","h","s","l","e","y","z"]
+# d.sort(reverse=True)
+# print(d)
+
+#2.CHANGE THE ORDER OF NUMBERS & STRINGS
+# a = [1 , 2, 3, 4]
+# a.reverse()
+# print(a)
+#
+# b = "SAM"
+# b.reverse()
+# print(b)
+
+#3.ADD ANY NUMBER OR STRING IN A LIST
+# a = ["Hope","Foundation","Ai"]
+# b = ["Skill","Training","Center"]
+# a.extend(b)
+# print(a)
+#
+# a = [1,2,3]
+# b = [4,5,6]
+# a.extend(b)
+# print(a)
+
+#4.FINE NUMBER OF ELEMENTS
+# a = ["a","b","c","d","a","c","a","d"]
+# b = a.count("a")
+# print(b)
+
+#5.FIND THE LARGEST & SMALLEST NUMBER IN LIST
+# a = [9,5,1,2,4,7]
+# print(max(a))
+# print(min(a))
+
+# #6.ACCESS THE LIST
+# a = ["a","b","c","d"]
+# a.insert(1,"H")
+# print(a)
+
+#7.MODIFY THE LIST
+# a = ["a","b","c","d"]
+# a[0]="e"
+# print(a)
+
+# a = [1,2,3,4,5]
+# a[1]="5"
+# print(a)
+
+#8.CONCENATE (ADD) THE  LIST
+
+# a = [1,2,3]
+# b = [4,5,6]
+#
+# print(a+b)
+
+#9.REMOVE DUPLICATE
+# a = [1,1,2,1,3,4,1,2,5,5,6,6,3,4]
+# b = (set(a))
+# print(b)
+#
+# a = ["AAA","BBB","AAA","DDD","CCC","AAA","CCC","BBB","DDD","EEE"]
+# b = (set(a))
+# print(b)
+
+#10.IDENTIFY 1ST AND LAST ELIMENT IN LIST
+
+a = input("first eliment in list :")
+
+b = input(a[0])
+c = input("last eliment in list :")
+d = input(c[-1])
+print(b,c)
+
+
+
+
+
