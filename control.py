27eefb1fdc56e137138bf57fa3c77c9a2c8d78ce
NAGARJUNA ETHAMUKKALA <120629706+NagaRjuna44@users.noreply.github.com>
2024-02-29 16:05:06 +0530

    Add files via upload

---
+++ control.py
@@ -0,0 +1,23 @@
+a = int(input("Enter the number:"))
+if a==0:
+    print("The Number Is Zero")
+elif a == 1:
+    print("The Number Is One")
+elif a == 2:
+    print("The Number Is Two")
+elif a == 3:
+    print("The Number Is Three")
+elif a == 4:
+    print("The Number Is Four")
+elif a == 5:
+    print("The Number Is Five")
+elif a == 6:
+    print("The Number Is Six")
+elif a == 7:
+    print("The Number Is Seven")
+elif a == 8:
+    print("The Number Is Eight")
+elif a == 9:
+    print("The Number Is Nine")
+else:
+    print("Enter The Valide Number")
