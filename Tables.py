95433ae1f41f4b1213042a86bde508ab141b183b
NAGARJUNA ETHAMUKKALA <120629706+NagaRjuna44@users.noreply.github.com>
2024-03-04 20:05:55 +0530

    Add files via upload

---
+++ Tables.py
@@ -0,0 +1,10 @@
+################################################################################
+
+a = int(input("Enter The Number:"))
+b = int(input("Enter The Number:"))
+c = int(input("Enter The Number:"))
+d = int(input("Enter The Number:"))
+e = int(input("Enter The Number:"))
+for i in range (1,10+1) :
+    print(f"{a} * {i} = {i * a}     {b} * {i} = {i * b}     {b} * {i} = {i * b}     {c} * {i} = {i * c}     {d} * {i} = {i * d}     {e} * {i} = {i * e}")
+
